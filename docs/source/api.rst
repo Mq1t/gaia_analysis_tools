337cdb28077681e624fdb5996e90913c59991bd9
Mq1t <matthewmcg815@gmail.com>
2026-08-12 10:07:26 -0400

    Autodocs

---
+++ docs/source/api.rst
@@ -0,0 +1,7 @@
+API Reference
+=============
+
+.. automodule:: gaia_analysis_tools
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,10 +11,18 @@ copyright = '2026, Matthew McGuire, Jennifer Farrell, Nakia McKay'
 author = 'Matthew McGuire, Jennifer Farrell, Nakia McKay'
 release = '1.0'
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../.."))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
